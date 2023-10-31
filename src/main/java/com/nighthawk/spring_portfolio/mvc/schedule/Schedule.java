2c4b09f09917655bff920d18d23159a311ce288c
Orlando-c <lando.j.carcamo@gmail.com>
2023-10-31 10:44:34 -0700

    update schedule

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/schedule/Schedule.java
@@ -0,0 +1,22 @@
+package com.nighthawk.spring_portfolio.mvc.schedule;
+
+public class ScheduleData {
+    private String classPeriod1;
+    private String classPeriod2;
+    private String classPeriod3;
+    private String classPeriod4;
+    private String classPeriod5;
+
+    // Add constructors, getters, and setters as needed
+
+    // Example implementation
+    public String getClassPeriod1() {
+        return classPeriod1;
+    }
+
+    public void setClassPeriod1(String classPeriod1) {
+        this.classPeriod1 = classPeriod1;
+    }
+
+    // Repeat the above getter and setter methods for classPeriod2, classPeriod3, classPeriod4, and classPeriod5
+}
